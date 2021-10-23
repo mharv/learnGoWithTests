55b1eb7bf1389265436584aedb9f3532d1fb9592
Mitchell Harvey <mitch.k.harvey@gmail.com>
2021-10-23 18:07:07 +1100

    rewrite tests for functions to accept rectangle define rectangle struct

--- structsMethodsInterfaces/structsMethodsInterfaces.go
+++ structsMethodsInterfaces/structsMethodsInterfaces.go
@@ -4,10 +4,15 @@ func main() {
 
 }
 
-func Perimeter(height, width float64) float64 {
-	return 2 * (height + width)
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
 
-func Area(height, width float64) float64 {
-	return height * width
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Height + rectangle.Width)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Height * rectangle.Width
 }
