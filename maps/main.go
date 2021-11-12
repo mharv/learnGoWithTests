483bf524ed32c6a76b7e095f98aa876678e754c6
Mitchell Harvey <mitch.k.harvey@gmail.com>
2021-11-12 23:10:35 +1100

    add error errworddoesnotexist flesh out Updatre method to check if word being updated exists

--- maps/main.go
+++ maps/main.go
@@ -9,8 +9,9 @@ func main() {
 }
 
 const (
-	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
 
 type DictionaryErr string
@@ -46,7 +47,15 @@ func (d Dictionary) Add(word, definition string) error {
 }
 
 func (d Dictionary) Update(word, definition string) error {
-	d[word] = definition
-	return nil
+	_, err := d.Search(word)
 
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+	return nil
 }
