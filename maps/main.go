3545cc0f35cc08909d0893f71513856f5f9fd025
Mitchell Harvey <mitch.k.harvey@gmail.com>
2021-11-12 22:05:00 +1100

    add conditions in tests for unknown words in map make tests pass by accessing the second returned variable from the map lookup

--- maps/main.go
+++ maps/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("hello world")
@@ -8,6 +11,12 @@ func main() {
 
 type Dictionary map[string]string
 
-func Search(dictionary Dictionary, key string) string {
-	return dictionary[key]
+func Search(dictionary Dictionary, key string) (string, error) {
+
+	definition, ok := dictionary[key]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return definition, nil
 }
