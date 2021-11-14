f41a5b3ab9947d400002c5d36d0c34d4f8f2bd3a
Mitchell Harvey <mitch.k.harvey@gmail.com>
2021-11-14 19:48:59 +1100

    refactor to use io.Writer

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
 )
 
 func main() {
 	fmt.Println("test")
 }
 
-func Countdown(out *bytes.Buffer) {
+func Countdown(out io.Writer) {
 	fmt.Fprint(out, "3")
 }
